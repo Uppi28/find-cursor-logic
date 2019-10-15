5a5fd8d9ee8ac23e36e14b85e16f75508dfdd8ec
Upendra Kumar B R <Upendra.BR@mu-sigma.com>
2019-10-15 20:55:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'autocomplete';
+export class AppComponent implements OnInit {
+  private query;
+  private i;
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.query = document.getElementById('query');
+    this.query.addEventListener('input', (event) => this.getCursorText(event));
+    
+  }
+
+  getCursorText(event) {
+      let i;
+      let value = event.srcElement.value;
+      
+      for(i = this.query.selectionStart-1; i>=0; i--) {
+          if(value[i] === " ") {
+              console.log(value[i]);
+              
+              break;
+          }
+      }
+      i++;
+      const word = value.slice(i).split(" ")[0];
+      
+      return word;
+  }
 }
